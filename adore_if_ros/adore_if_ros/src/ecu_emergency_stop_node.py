3441cb94b2da732096f2bbc7d3d68d380c858a38
Andrew Koerner <andrew.koerner@dlr.de>
2023-03-14 11:48:02 +0100

    implemented emergency stop node

--- adore_if_ros/adore_if_ros/src/ecu_emergency_stop_node.py
+++ adore_if_ros/adore_if_ros/src/ecu_emergency_stop_node.py
@@ -10,34 +10,63 @@
 # * SPDX-License-Identifier: EPL-2.0 
 # *
 # * Contributors: 
-# *	  Thomas Lobig
+# *      Thomas Lobig
 # ********************************************************************************
 
 import rospy
+import time
 
 from std_msgs.msg import Bool
 
 
-def callback_sample(msg):
-	print(msg.data)
+pub = None
 
-    
-def signal_publish():
-	# if condition?:
-	pub.publish(False)
-	# False does nothing, unless you wanted to start already stopped automation
-	# True will make it so the tactical planner stops sending trajectories to the feedback loop controller
-	
+def callback_sample(msg):
+    print(msg.data)
+
+
+emergency_stop=False
+
+def get_brake_pedal_state():
+    current_state = False
+    with open('/var/log/rsyslog/telemetry.log', 'r') as f:
+        last_line = f.readlines()[-1]
+        current_state = bool(int(last_line.split("brake_pedal_state:")[1].strip()))
+    return current_state
 
 if __name__ == '__main__':
-	pub = rospy.Publisher('/vehicle0/FUN/HaltAutomation', Bool, queue_size=1)
-	rospy.init_node('ecu_emergency_stop_node')
-	
+    pub = rospy.Publisher('/vehicle0/FUN/HaltAutomation', Bool, queue_size=1)
+    rospy.init_node('ecu_emergency_stop_node')
+    # run loop:
+    r = rospy.Rate(10) # 10hz
+    
+    print("ECU Emergency stop node starting...", flush = True)
+    current_state=False
+    last_state=False
+    while True:
+        if rospy.is_shutdown():
+            continue
+        current_state = get_brake_pedal_state()
+        if current_state != last_state:
+            print(f"  brake pedal state: {current_state}", flush = True)
+            pub.publish(current_state)
+        r.sleep()
+        last_state = current_state
+    #    time.sleep(.1)
+    #    with open('/var/log/rsyslog/telemetry.log', 'r') as f:
+    #        last_line = f.readlines()[-1]
+    #        current_state = bool(int(last_line.split("brake_pedal_state:")[1].strip()))
+    #    print("emergency_stop: {current_state}")
+        #while not rospy.is_shutdown():
+        #    with open('/var/log/rsyslog/telemetry.log', 'r') as f:
+        #        last_line = f.readlines()[-1]
+        #        current_state = bool(int(last_line.split("brake_pedal_state:")[1].strip()))
+        #    print(f"Publishing emergency_stop: {current_state}")
+        #    signal_publish()
+        #    pub.publish(current_state)
+        #    r.sleep()
+        #    last_state=current_state
 
-	# run loop:
-	r = rospy.Rate(10) # 10hz
-	while not rospy.is_shutdown():
-		signal_publish()
-		r.sleep()
 
-	# alternatively use ros.spin() to wait until shutdown, if you don't need a run loop
+
+    # alternatively use ros.spin() to wait until shutdown, if you don't need a run loop
